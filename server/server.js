d35618e9974328ed94aa9d8670e2f29ed55529ed
Vishnu <vishnuchen@gmail.com>
2019-07-24 15:39:14 -0400

    got users connected using sockets

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected1');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3001, function(){
