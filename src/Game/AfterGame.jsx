ef64d3c07991235a8aa9d5a20d087106331428e3
Vishnu <vishnuchen@gmail.com>
2019-07-27 15:47:05 -0400

    added scoreboard css in the game results in the end

--- src/Game/AfterGame.jsx
+++ src/Game/AfterGame.jsx
@@ -7,13 +7,27 @@ class AfterGame extends Component {
 
   render() {
     return (
-      <div>
-        <div className="results-area">
-          <h1>Game Results</h1>
-          <h3>You scored: {this.props.score} points!</h3>
-          <a href="/">Want to play again?</a>
-        </div>
+      <div id="container">
+      <div class="row">
+        <div class="name">Player1</div><div class="position">1</div>
       </div>
+
+      <div class="row">
+        <div class="name">Player2</div><div class="position">2</div>
+      </div>
+
+      <div class="row">
+        <div class="name">Player3</div><div class="position">3</div>
+      </div>
+
+      <div class="row">
+        <div class="name">Player4</div><div class="position">4</div>
+      </div>
+
+      <div class="row">
+        <div class="name">Player5</div><div class="position">5</div>
+      </div>
+    </div>
     );
   }
 }
